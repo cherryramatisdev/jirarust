b66728d7085b45be83cc186538b8142970f233cd
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-03-14 21:48:16 -0300

    Add action to move card to review column and create PR

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -2,6 +2,7 @@ pub mod get_jira_config;
 pub mod get_jira_pr_title;
 pub mod get_jira_title;
 pub mod progress_card;
+pub mod review_card;
 pub mod transitions;
 pub mod assignee_card;
 pub mod create_new_branch;
---
+++ src/actions/review_card.rs
@@ -0,0 +1,26 @@
+use crate::actions::transitions::TRANSITIONS;
+use crate::git_api;
+use crate::{actions, jira_api};
+use std::error;
+
+pub fn call(code: &usize) -> Result<bool, Box<dyn error::Error>> {
+    let transition_response =
+        jira_api::move_card_status::call(code, &TRANSITIONS.review).unwrap();
+    let branches = git_api::list_branches::call().unwrap();
+    let has_develop = branches.iter().any(|s| s == "develop");
+    let pr_title = actions::get_jira_pr_title::call(code).unwrap();
+
+    if transition_response.status_code == 204 {
+        let base_branch = if has_develop { "develop" } else { "main" };
+
+        let status = git_api::create_pr::call(&base_branch.to_string(), &pr_title);
+
+        if status.success() {
+            git_api::view_current_pr::call()?;
+        }
+
+        print!("{}", status);
+    }
+
+    Ok(true)
+}
