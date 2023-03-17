4c913f0633be7589511275812d9b87a6e2464f56
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-03-16 21:38:14 -0300

    Consume provider as part of a command with branch types

--- src/actions/progress_card.rs
+++ src/actions/progress_card.rs
@@ -10,12 +10,12 @@ struct Transition {
     id: String,
 }
 
-pub fn call(code: &usize) -> Result<bool, Box<dyn std::error::Error>> {
+pub fn call(branch_type: &str, code: &usize) -> Result<bool, Box<dyn std::error::Error>> {
     let transition_response = jira_api::move_card_status::call(code, &TRANSITIONS.progress)?;
 
     let assignee_response = actions::assignee_card::call(code).unwrap();
 
-    let branch_response = git_api::create_branch::call(code).unwrap();
+    let branch_response = git_api::create_branch::call(branch_type, code).unwrap();
 
     let all_good = transition_response.status_code == 200
         && assignee_response.status_code == 200
