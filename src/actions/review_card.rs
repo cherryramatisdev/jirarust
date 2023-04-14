d4f9118a6ec12336a9dc0ab79b744cd74fb746d1
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-04-14 19:00:11 -0300

    Allow creation of PR without card

--- src/actions/review_card.rs
+++ src/actions/review_card.rs
@@ -4,28 +4,41 @@ use crate::jira_api::transitions::TRANSITIONS;
 use crate::log::{log, LogType};
 use crate::{actions, jira_api};
 
-pub fn call(code: &usize) -> Result<bool, Error> {
-    let transition_response = jira_api::move_card_status::call(code, &TRANSITIONS.review).unwrap();
+pub fn call(code: &Option<usize>) -> Result<bool, Error> {
     let branches = git_api::list_branches::call().unwrap();
     let has_develop = branches.iter().any(|s| s == "develop");
-    let pr_title = actions::get_pr_title::call(code).unwrap();
+    let base_branch = if has_develop { "develop" } else { "main" };
+
+    if code.is_none() {
+        return create_pr(&base_branch.to_string(), &String::new());
+    }
+
+    let code = code.unwrap();
+    let transition_response = jira_api::move_card_status::call(&code, &TRANSITIONS.review).unwrap();
+    let pr_title = actions::get_pr_title::call(&code).unwrap();
 
     if transition_response.status_code == 204 {
-        let pr_exists = git_api::pr_exist::call(&git_api::pr_exist::ViewCurrentPrCommand)?;
-
-        if !pr_exists {
-            let base_branch = if has_develop { "develop" } else { "main" };
-
-            let status = git_api::create_pr::call(&base_branch.to_string(), &pr_title);
-            if status.success() {
-                git_api::view_current_pr::call()?;
-                log(LogType::Info, format!("{}", status).as_str());
-            }
-        } else {
-            git_api::view_current_pr::call()?;
-            log(LogType::Info, "PR already exists");
-        }
+        return create_pr(&base_branch.to_string(), &pr_title);
     }
 
     Ok(true)
+}
+
+fn create_pr(base_branch: &String, pr_title: &String) -> Result<bool, Error> {
+    let pr_exists = git_api::pr_exist::call(&git_api::pr_exist::ViewCurrentPrCommand)?;
+
+    if pr_exists {
+        log(LogType::Info, "PR already exists");
+        git_api::view_current_pr::call()?;
+        return Ok(true);
+    }
+
+    let status = git_api::create_pr::call(base_branch, pr_title);
+    if status.success() {
+        log(LogType::Info, format!("{}", status).as_str());
+        git_api::view_current_pr::call()?;
+        return Ok(true);
+    }
+
+    Ok(false)
 }
