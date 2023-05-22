d548feebde0c71e555aad46baf5cbe149fb21b19
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-05-22 18:35:44 -0300

    chore: sort mod

--- src/git_api/mod.rs
+++ src/git_api/mod.rs
@@ -1,10 +1,10 @@
 pub mod branch_exist;
-pub mod has_develop;
 pub mod change_to_branch;
 pub mod command_trait;
 pub mod create_pr;
 pub mod get_current_branch;
 pub mod get_current_jira_code;
+pub mod has_develop;
 pub mod list_branches;
 pub mod pr_exist;
 pub mod view_current_pr;
